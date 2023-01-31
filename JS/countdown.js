77d53290dd8c7edbb875e6ca3c13cf9b485ff478
Juan Montejo <juan.montejo8@hotmail.com>
2023-01-31 18:36:37 -0500

    Actualización 31/01/2023 fecha 4

--- JS/countdown.js
+++ JS/countdown.js
@@ -1,9 +1,9 @@
 simplyCountdown('#cuenta', {
 	year: 2023, // required
-	month: 1, // required
-	day: 29, // required
-	hours: 12, // Default is 0 [0-23] integer
-	minutes: 20, // Default is 0 [0-59] integer
+	month: 2, // required
+	day: 12, // required
+	hours: 9, // Default is 0 [0-23] integer
+	minutes: 30, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
 		days: 'Día',
