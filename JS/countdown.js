947a4eb299c312698c7ba62bb47659f269ee7993
Juan Montejo <juan.montejo8@hotmail.com>
2023-02-21 20:45:24 -0500

    Actualización fecha 1 libertadores 21/02/2023

--- JS/countdown.js
+++ JS/countdown.js
@@ -1,7 +1,7 @@
 simplyCountdown('#cuenta', {
 	year: 2023, // required
 	month: 2, // required
-	day: 12, // required
+	day: 23, // required
 	hours: 13, // Default is 0 [0-23] integer
 	minutes: 0, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
