4ebfe5b8119b0e0943585ce5532126c15d81275e
JuanMontejo32 <90806289+JuanMontejo32@users.noreply.github.com>
2024-09-24 16:56:40 -0500

    Update countdown.js
    HP

--- JS/countdown.js
+++ JS/countdown.js
@@ -1,9 +1,9 @@
 simplyCountdown('#cuenta', {
-	year: 2023, // required
-	month: 2, // required
-	day: 23, // required
-	hours: 13, // Default is 0 [0-23] integer
-	minutes: 0, // Default is 0 [0-59] integer
+	year: 2024, // required
+	month: 10, // required
+	day: 4, // required
+	hours: 4, // Default is 0 [0-23] integer
+	minutes: 44, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
 		days: 'Día',
